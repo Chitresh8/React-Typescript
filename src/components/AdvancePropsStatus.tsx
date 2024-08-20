3bc861338e01214b3c47de7114eaf349e26a861b
Chitresh8 <chitreshbabu8@gmail.com>
2024-08-21 00:07:39 +0530

    feat(file):type script demo practice project advancedprops file

--- src/components/AdvancePropsStatus.tsx
+++ src/components/AdvancePropsStatus.tsx
@@ -1,1 +1,16 @@
-export const AdvancePropsStatus = () => {};
+//without typescript
+// export const AdvancePropsStatus = () => {
+//   return (
+//     <div>
+//       <h2>Loading.............</h2>
+//       <h2>Data fetched successfully</h2>
+//       <h2>Error fetching data</h2>
+//     </div>
+//   );
+// };
+
+//with typescript
+
+export const AdvancePropsStatus = () => {
+  return <></>;
+};
